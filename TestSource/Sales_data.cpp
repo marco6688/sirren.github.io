766ee3ddb8739fa8abe1191f51d64850f6f9cf56
renzhentao <18516066415@163.com>
2019-03-27 15:43:14 +0800

    add note

---
+++ TestSource/Sales_data.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 任振涛 on 2019/3/20.
+//
+
+struct Sale_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
---
+++ TestSource/test.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main(){
+
+//   vector<string> v5(10,'100');
+//    vector<string> mygoal = { "Try", "my", "best" };
+   vector<int> v6(10,1);
+
+    for(int i=0; i<v6.size(); ++i)
+    {
+        std::cout << v6[i] << ' ';
+    }
+
+//    while(cin>>line){
+//        cout<< "hi:" << endl;
+//        cout<< line << endl;
+//    }
+    return 0;
+}
+
+//int iPrint(vector aaa){
+//    for(int i=0; i<aaa.size(); ++i)
+//    {
+//        std::cout << aaa[i] << ' ';
+//    }
+//    return  0;
+//}
+
+
+
