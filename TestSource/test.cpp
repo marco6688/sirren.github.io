933676deb88bd4def3eb5b9a5ba220d4f48aca65
renzhentao <18516066415@163.com>
2019-04-08 16:22:25 +0800

    add note

--- TestSource/test.cpp
+++ TestSource/test.cpp
@@ -7,17 +7,28 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+int reset(int *p1,int *p2);
+
+int sum_num(vector<int> arr,int &cut );
 
 int main(){
 
 //   vector<string> v5(10,'100');
 //    vector<string> mygoal = { "Try", "my", "best" };
-   vector<int> v6(10,1);
+//int a1 = 10,b1 = 20;
+//
+//reset(&a1,&b1);
+//cout<<a1<<"-"<<b1<<endl;
+vector<int> aa;
+aa.push_back(1);
+aa.push_back(2);
+aa.push_back(3);
+aa.push_back(4);
+int num = 0;
 
-    for(int i=0; i<v6.size(); ++i)
-    {
-        std::cout << v6[i] << ' ';
-    }
+int a1 = sum_num(aa,num);
+std::cout << a1 << ' '<<endl;
+std::cout << num << ' '<<endl;
 
 //    while(cin>>line){
 //        cout<< "hi:" << endl;
@@ -35,4 +46,25 @@ int main(){
 //}
 
 
+int reset(int *p1,int *p2){
 
+//    *p = 0;
+//    cout<<p<<endl;
+//    cout<<&p<<endl;
+//    cout<<*p<<endl;
+
+    int p = 0;
+    p =  *p1;
+    *p1 = *p2;
+    *p2 = p;
+    return 0;
+}
+
+int sum_num(vector<int> arr,int &cut ){
+    int num = 0;
+    for(int i=0;i<arr.size();i++){
+        num += arr[i];
+        cut++;
+    }
+    return num;
+}
